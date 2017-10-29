cfeaf986abd6a36676329e4db5717cb84b1da694
Pierre Laroze <pierre.laroze@live.fr>
2017-10-29 21:12:28 +0100

    add search bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :books
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :books, only: [ :index ]
